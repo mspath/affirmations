3a926a9e032b87043d4138082a32072392ac1ed2
Markus Spath <markus@hackr.de>
2022-01-21 12:54:13 +0100

    update AS and add tests

---
+++ app/src/androidTest/java/com/example/affirmations/AffirmationsListTests.kt
@@ -0,0 +1,43 @@
+package com.example.affirmations
+
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+
+@RunWith(AndroidJUnit4::class)
+class AffirmationsListTests {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.example.affirmations", appContext.packageName)
+    }
+
+    @Test
+    fun scroll_to_item() {
+
+        onView(withId(R.id.recycler_view)).perform(
+            RecyclerViewActions
+                .scrollToPosition<RecyclerView.ViewHolder>(9))
+
+        onView(withText(R.string.affirmation10))
+            .check(matches(isDisplayed()))
+
+    }
+}
